cc244f156fc75e1942c014dcd9c4d53fa79998c8
fatih <afyildiz@gmail.com>
2023-07-26 01:05:46 +0300

    mail send ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactmeComponent } from './comps/contactme/contactme.component';
 import { HomeComponent } from './comps/home/home.component';
+import { ProjectsComponent } from './comps/projects/projects.component';
 
 const routes: Routes = [
   {path:'contact',component:ContactmeComponent},
+  {path:'projects',component:ProjectsComponent},
   {path:'',component:HomeComponent},
 ];
 

bf60aa8f9bee97642c557fd35a08c8b50498cd9d
fatih yildiz <afyildiz@gmail.com>
2024-09-21 14:57:08 +0300

    update

--- src/app/comps/skills/skills.component.ts
+++ src/app/comps/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  backgroundPosition = '50% 50%';
 
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const xOffset = (event.clientX - centerX) / centerX * 10;
+    const yOffset = (event.clientY - centerY) / centerY * 10;
+    this.backgroundPosition = `${50 + xOffset}% ${50 + yOffset}%`;
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  openLink(url:string){
+    window.open(url, '_blank');
+  }
 }
